5288f02c5825918828f0dcf84b94c1ad95806084
Simon <simon-enns@web.de>
2025-12-04 00:44:31 +0100

    added optional info logging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
 
 
@@ -13,8 +14,13 @@ def main():
         epilog="Hope this helped..."
     )
     parser.add_argument("prompt", type=str, help="User Prompt")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="Use verbose output")
     args = parser.parse_args()
     prompt = args.prompt
+    with_verbose = args.verbose
+
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -22,17 +28,18 @@ def main():
         raise RuntimeError("'GEMINI_API_KEY' not found!")
 
     client = genai.Client(api_key=api_key)
-    # prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=prompt)
+        model="gemini-2.5-flash", contents=messages)
 
     metadata = response.usage_metadata
     if metadata is None:
         raise RuntimeError("Failed API request: Response is None")
 
-    print(f"Prompt tokens: {metadata.prompt_token_count}")
-    print(f"Response tokens: {metadata.candidates_token_count}")
+    if with_verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {metadata.prompt_token_count}")
+        print(f"Response tokens: {metadata.candidates_token_count}")
     print(response.text)
 
 
